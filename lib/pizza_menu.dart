bdaeabcbb1d135fa7dc7128ea6dc0ebb0d4e312d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-08 09:29:46 -0300

    Adição de box e de imagens

--- lib/pizza_menu.dart
+++ lib/pizza_menu.dart
@@ -5,21 +5,57 @@ class PizzaMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text('Pizza Menu'),),
-      body:Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pizza Menu'),
+      ),
+      body: Column(
         children: [
           Card(
-            child: Row(
-              children: [
-                Image.asset(),
-              ],
+            elevation: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "images/focaccia.jpg",
+                    width: 100,
+                    height: 100,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Text 1",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Text 2",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      Text(
+                        "Text 3",
+                        style: TextStyle(
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ), 
+          ),
         ],
-      ) ,
-      );
-
-    
+      ),
+    );
   }
 }
