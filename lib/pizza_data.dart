054e55724e73de6fdc33a2597bc9a2c276c8662b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-15 09:07:47 -0300

    adição do pizza_data

---
+++ lib/pizza_data.dart
@@ -0,0 +1,60 @@
+class Pizza {
+  final String name;
+  final String ingredients;
+  final int price;
+  final String photoName;
+  final bool soldOut;
+
+  const Pizza({
+    required this.name,
+    required this.ingredients,
+    required this.price,
+    required this.photoName,
+    required this.soldOut,
+  });
+}
+
+const List<Pizza> pizzaData = [
+  Pizza(
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  ),
+  Pizza(
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  ),
+  Pizza(
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  ),
+  Pizza(
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  ),
+  Pizza(
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  ),
+  Pizza(
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  ),
+];
